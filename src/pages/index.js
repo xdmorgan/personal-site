8190815b6767d53f235097eb54edcdee277b76d4
Danny Morgan <dan@dannydev.com>
2018-12-28 12:08:12 -0500

    Update homepage copy and add Huge

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,23 @@ const IndexPage = () => (
     <section className="Hero">
       <div className="Container">
         <h1>
-          <span role="img" aria-label="Hello world">
-            👋 🌎
+          <span role="img" aria-label="Man using laptop">
+            👨‍💻
           </span>
         </h1>
+        <h2>Hi, I'm Dan.</h2>
+        <h3>
+          I'm a Senior Web Engineer at <a href="//hugeinc.com">Huge</a> in
+          Washington D.C.
+        </h3>
         <p>
-          I'm Dan. I build UIs in Washington, DC. Currently, UI Developer at{' '}
-          <a href="//cvent.com">Cvent</a>. Previously, Full-stack at{' '}
-          <a href="//prpl.rs">PRPL</a>, Application developer at{' '}
+          What does that mean? Well, I have experience writing, testing,
+          deploying (and debugging) JavaScript, Python, and Mobile applications.
+        </p>
+        <p>
+          Prior to Huge, I worked as a UI Developer at{' '}
+          <a href="//cvent.com">Cvent</a>, Full-stack Developer at{' '}
+          <a href="//prpl.rs">PRPL</a>, Application Developer at{' '}
           <a href="//gifn.it">Gifn</a>, and UI Designer at{' '}
           <a href="//xstudios.agency">X Studios</a>.
         </p>

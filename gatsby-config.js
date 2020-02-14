8e9a5cc70bd93f55c047f42ebb4a9b6b73712553
Dan Morgan <hello@danny.codes>
2020-02-13 21:34:14 -0500

    Upgrade theme to v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,22 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   siteMetadata: {},
   plugins: [
     {
-      resolve: 'gatsby-theme-xdmorgan',
+      resolve: "gatsby-theme-xdmorgan",
       options: {
         gtag: process.env.UA_TRACKING_ID,
-        gfonts: ['IBM Plex Sans:400,700', 'Montserrat:700'],
+        gfonts: ["IBM Plex Sans:400,700", "Montserrat:700"],
         filesystem: [
           {
             name: `images`,
-            path: `${__dirname}/src/images`,
+            path: `${__dirname}/src/images`
           },
           {
             name: `posts`,
-            path: `${__dirname}/src/content/posts`,
-          },
+            path: `${__dirname}/src/content/posts`
+          }
         ],
         manifest: {
           name: `Dan Morgan // UI Engineer`,
@@ -25,25 +25,25 @@ module.exports = {
           background_color: `#2b2b31`,
           theme_color: `#0068fa`,
           display: `minimal-ui`,
-          icon: `src/images/avatar.png`,
+          icon: `src/images/avatar.png`
         },
         seo: {
           // site meta description
-          description: process.env.SITE_META_DESCRIPTION || '',
+          description: process.env.SITE_META_DESCRIPTION || "",
           // site meta keywords
-          keywords: (process.env.SITE_META_KEYWORDS || '').split(', '),
+          keywords: (process.env.SITE_META_KEYWORDS || "").split(", "),
           // site logo (meta json schema)
-          logo: process.env.SITE_META_LOGO || '',
+          logo: process.env.SITE_META_LOGO || "",
           // organization name (meta json schema)
-          organization: process.env.SITE_META_ORGANIZATION || '',
+          organization: process.env.SITE_META_ORGANIZATION || "",
           // Base page title
-          title: process.env.SITE_META_TITLE || '',
+          title: process.env.SITE_META_TITLE || "",
           // twitter card user attribution
-          twitter: process.env.SITE_META_TWITTER || '',
+          twitter: process.env.SITE_META_TWITTER || "",
           // fkn base url
-          url: process.env.SITE_META_URL || '',
-        },
-      },
-    },
-  ],
-}
+          url: process.env.SITE_META_URL || ""
+        }
+      }
+    }
+  ]
+};

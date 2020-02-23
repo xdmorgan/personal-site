9ab385125ce886f4a72ce7e860b3e0213a682938
Dan Morgan <hello@danny.codes>
2020-02-23 15:01:56 -0500

    Feature/v2020 contact cta (#38)
    * Add contact section
    * Clean up homepage

---
+++ src/pages/styleguide/contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ContactSection } from '../../components'
+
+export default function Page() {
+  return (
+    <>
+      <div className="container pt-10x pb-4x md:pt-12x child-my-0">
+        <h1 className="h1">Contact Section</h1>
+      </div>
+      <ContactSection />
+    </>
+  )
+}

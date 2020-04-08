a532f704076b7965f77bc033f3401a803ec349cd
Dan Morgan <hello@danny.codes>
2020-04-07 21:16:12 -0400

    Update about wording (#85)
    * Update about section wording
    * Update rendering lists post content

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -33,54 +33,55 @@ export function AboutSection() {
           >
             <h2 className="h3 mb-2x">Skills &amp; Expertise</h2>
             <p className="small c-mystic">
-              Over my career, I&rsquo;ve helped Cartoon Network, Cvent, EA
-              Sports, Facebook, Huge, LinkedIn, NASA, Oculus, and Wayfair craft
-              digital products for mobile, web, and emerging platforms.
+              Over my career, I&rsquo;ve worked with companies including Cartoon
+              Network, Cvent, EA Sports, Facebook, Huge, LinkedIn, NASA, Oculus,
+              and Wayfair to craft digital products for mobile, web, and
+              emerging platforms.
             </p>
             <p className="small c-mystic">
-              In roles varying from UI designer to tech lead, I&rsquo;ve gained
-              valuable insight into a broad spectrum of technologies and
-              methodologies. In recent years, I&rsquo;ve focused on the
-              following skills to specialize in UI architecture and front end
-              engineering.
+              In roles varying from visual designer to tech lead, I&rsquo;ve
+              developed a holistic understanding of the software development
+              lifecycle. In recent years, I&rsquo;ve focused on the following
+              areas to specialize in UI architecture and front end engineering.
             </p>
           </div>
           <div className={cx(styles.grid__skills, styles.skills)}>
             <Skill title="Front End Development">
-              HTML, JavaScript, React, Angular, Vue, CSS, Sass,
+              HTML, JavaScript, React, Next.js, Gatsby, Vue, CSS, Sass,
               styled-components, SVG, animation, and HTML email
             </Skill>
-            <Skill title="Full Stack Development">
-              Node, Python, WordPress, Shopify, GraphQL, REST, Firebase, and
-              Relational &amp; Document DBs
-            </Skill>
             <Skill title="Design Systems">
-              Atomic design, design tokens, application theming, and ergonomic
-              component API design
-            </Skill>
-            <Skill title="Project Architecture">
-              AWS, GCP, Docker, NPM, JAMStack, headless CMS, serverless lambda
-              functions, and CI/CD pipelines
+              Atomic design, design tokens, application theming, library
+              versioning, component API design, and system evangelism
             </Skill>
             <Skill title="Testing &amp; Tooling">
-              Webpack, Jest, Prettier, ESLint, TypeScript, Storybook, Babel,
-              Parcel, Gulp, and Cypress
+              Webpack, Jest, Cypress, Prettier, ESLint, TypeScript, Storybook,
+              Babel, Parcel, Gulp, and custom tooling / CLIs
+            </Skill>
+            <Skill title="Full Stack Development">
+              Node, Python, WordPress, Shopify, GraphQL, REST, Firebase,
+              Relational and Document DB architectures
             </Skill>
             <Skill title="Mobile &amp; Emerging Tech">
-              Swift, React Native, Xamarin (C#), PWAs, Actions on Google, and
-              Dialogflow
+              Swift, React Native, Progressive Web Apps, Actions on Google,
+              Chatbots, Dialogflow, and Xamarin (C#)
+            </Skill>
+            <Skill title="Architecture & Ops">
+              AWS, GCP, Docker, NPM, JAMStack, headless CMS, serverless/lambda
+              functions, and CI/CD pipelines
             </Skill>
             <Skill title="Accessibility &amp; i18n">
               WCAG 2.1 &amp; Section 508 guidelines, ARIA accessible
-              applications, axe, JAWS, Wave, and Accessibility Insights for Web
-            </Skill>
-            <Skill title="Project Management">
-              Git, GitHub, BitBucket, JIRA, Trello, Asana, kanban &amp; scrum
-              agile processes
+              applications, axe testing, JAWS, Wave, and Accessibility Insights
+              for Web
             </Skill>
             <Skill title="Design Tools &amp; Hand-off">
               Sketch, Figma, Photoshop, Illustrator, Zeplin, Invision, asset
               optimization, Abstract, and Framer X
+            </Skill>
+            <Skill title="Project Management">
+              Git, GitHub, BitBucket, JIRA, Trello, Asana, kanban and scrum
+              agile methodologies
             </Skill>
             <Skill title="Team Leadership" className="d-block md:d-none">
               Architecture, mentorship, code reviews, presentations, data flow
